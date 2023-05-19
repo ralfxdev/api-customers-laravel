29c372e21344c63ac8b2d92fe66fd7f1844a305f
ralfxdev <lopezalexander1019@gmail.com>
2023-05-18 21:04:50 -0600

    transforming database data into json

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -17,14 +17,14 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
-        $status = $this->faker->randomElement(['B'. 'P', 'V']);
+        $status = $this->faker->randomElement(['B', 'P', 'V']);
 
         return [
             'customer_id' => Customer::factory(),
             'amount' => $this->faker->numberBetween(100, 20000),
             'status' => $status,
-            'billed_dated' => $this->faker->dateTimeThisDecade(),
-            'paid_dated' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
         ];
     }
 }
